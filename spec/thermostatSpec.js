3eb56e232ee41d2f4669b9dfb4a9dba2b58aee57
Chris Russell <chris.john.russell@gmail.com>
2018-06-15 15:27:21 +0100

    basic thermostat

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('turns the temperature up', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('turns the temperature down', function() {
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+});
